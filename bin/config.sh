7962b54f06d5f8059cf4c22c111cd617b19842bf
Vangelys <guilherme.razet@gmail.com>
2018-06-08 10:23:12 +0200

    init commit

---
+++ bin/config.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+######################## ENVIRONMENT VARIABLES ###############################
+######### change the following according to your own local setup #############
+
+
+# assumes this script (config.sh) lives in "${BASE_DIR}/semafor/bin/"
+export BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." > /dev/null && pwd )"
+# path to the absolute path
+# where you decompressed SEMAFOR.
+export SEMAFOR_HOME="${BASE_DIR}/semafor"
+
+export CLASSPATH=".:${SEMAFOR_HOME}/target/Semafor-3.0-alpha-04.jar"
+#:/home/guilherme/.m2/repository/commons-io/commons-io/2.4/commons-io-2.4.jar:/home/guilherme/git_repository/CoreNLP/src/edu/stanford/nlp/process/Morpha.java:${SEMAFOR_HOME}/target/morpha-stemmer-1.0.3.jar:/home/guilherme/.m2/repository/org/pcollections/pcollections/2.1.2/pcollections-2.1.2.jar
+
+# Change the following to the bin directory of your $JAVA_HOME
+export JAVA_HOME_BIN="/usr/bin"
+
+# Change the following to the directory where you decompressed 
+# the models for SEMAFOR 2.0.
+export MALT_MODEL_DIR="${SEMAFOR_HOME}/models/semafor_malt_model_20121129"
+export TURBO_MODEL_DIR="${SEMAFOR_HOME}/models/turbo_20130606"
+
+
+
+######################## END ENVIRONMENT VARIABLES #########################
+
+echo "Environment variables:"
+echo "SEMAFOR_HOME=${SEMAFOR_HOME}"
+echo "CLASSPATH=${CLASSPATH}"
+echo "JAVA_HOME_BIN=${JAVA_HOME_BIN}"
+echo "MALT_MODEL_DIR=${MALT_MODEL_DIR}"
---
+++ bin/runCoreNLP.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e #fail last
+
+MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
+
+source "${MY_DIR}/config.sh"
+
+if [ $# -lt 2 -o $# -gt 2 ]; then
+   echo "USAGE: `basename "${0}"` <input-file> <output-dir>"
+   exit 1
+fi
+
+# location of input file. must be absolute path
+INPUT_FILE="${SEMAFOR_HOME}/data/${1}"
+
+# where to write the output
+OUTPUT_DIR="${2}"
+
+TEST_PARSED_FILE="${OUTPUT_DIR}/conll"
+
+echo "**********************************************************************"
+echo "Running CoreNLP...."
+#-props StanfordCoreNLP-arabic.properties
+pushd ${SEMAFOR_HOME}/CoreNLP
+time java -mx2g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLP -props StanfordCoreNLP-french.properties -file ${INPUT_FILE} -outputDirectory ${TEST_PARSED_FILE} -outputFormat conllu
+echo "Finished running CoreNLP."
+echo "**********************************************************************"
+echo
+echo
